3e26a6e2b13e939d2b4c10d7f1b9a82b62b43926
FRANCESCA TONETTI <francesca.tonetti@studio.unibo.it>
2020-09-16 16:42:57 +0200

    To do:QuizRace

--- src/main/scala/QuizRaceUtils.scala
+++ src/main/scala/QuizRaceUtils.scala
@@ -1,12 +1,21 @@
 import model.QuestionManager
 import untitled.goose.framework.model.entities.runtime.GameState
 import untitled.goose.framework.model.events.GameEvent
-import untitled.goose.framework.model.events.consumable.ConsumableGameEvent
+import untitled.goose.framework.model.events.consumable.{ConsumableGameEvent, StepMovementEvent}
+
+import scala.util.Random
 
 trait QuizRaceUtils {
 
   private val questionManager : QuestionManager = QuestionManager(???)
+  val specialGroup = "SpecialGroup"
+  var tiles = Seq[Int]
+  val playerBetEvent: String = "PlayerBetEvent"
 
+  def getSpecialGroup(total: Int, numSpecialTile: Int): Seq[Int] = {
+    (1 until numSpecialTile).map(_ => Random.nextInt(total))
+    //metto il seed sul random
+  }
 
   def getTileGroups(total: Int) : Map[String, Seq[Int]] = {
     val groupSize = total / questionManager.availableCategories.size
@@ -14,8 +23,8 @@ trait QuizRaceUtils {
   }
 
   //TODO change with event
-  def getQuestion(category: String, difficulty: String, event: ConsumableGameEvent, gameState: GameState):
-  (String, String, Seq[(String, GameEvent)]) = {
+  def getQuestion(category: String, difficulty: String, bet: Int, event: ConsumableGameEvent, gameState: GameState):
+  (String, String, Seq[(String, StepMovementEvent)]) = {
     ???
   }
 }
